9d7e2ac9cc6353f42269451066d5155b89fca558
Matt Blomquist <blomquist.matt@gmail.com>
2017-09-29 10:19:39 -0700

    Add files via upload

---
+++ src/energy.cpp
@@ -0,0 +1,1 @@
+// Energy Equation - Initialization
---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+// Accel-CFD Main File
+
+// Add dependencies
+#include <iostream>
+#include <math.h>
+
+
+// Load Data from Input File
+
+// Initialize Output File
+
+// Initialize Continuity
+
+// Initialize Velocity
+
+// Initialize Energy Equation
+
+// Initialize Convergence Parameters
+
+// Run Transient SIMPLER Algorithm
+
+// Exit Program
